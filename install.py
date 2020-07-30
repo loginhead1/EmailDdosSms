b0037a1a61eeade19ead33a5aa68869f5fc8847a
deadrose <64653438+loginhead1@users.noreply.github.com>
2020-07-30 22:07:47 +0400

    Update install.py

--- install.py
+++ install.py
@@ -1,6 +1,7 @@
 import os
 os.system("pip install colorama")
 os.system("pip install requests")
+os.system("pip install pysocks")
 os.system("cd")
 os.system("cd EmailDdosSms")
 os.system("python spammer.py")
